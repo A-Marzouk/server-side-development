5f481db88787557cf4094fb51a9558362c4add41
Ahmed-pc <perfect_ahmed@yahoo.com>
2019-07-25 01:30:32 +0300

    callbacks

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 let shapes = require('./shapes');
 
 function solveRectangle(l,b) {
-    console.log('\n')
-    console.log("Solving for rectangle with l = "  +  l + " and b =" + b) ;
+    console.log('\n') ;
+    console.log("Solving for rectangle with l="  +  l + " and b=" + b) ;
 
-    if(l <=0 || b <= 0 ){
-        console.log('Rectangle dimensions should be greater than Zero : l= ' +l + ', b = ' + b);
-    }
-    else {
-        console.log("The area of rectangle is : " + shapes.rectangle.area(l,b) ) ;
-        console.log("The perimeter of rectangle is : " + shapes.rectangle.perimeter(l,b) ) ;
-    }
+    shapes(l,b, (error , data ) => {
+        // just like Axios !
+        if(error){ // error is not null
+            console.log('Error : ' + error);
+        }
+        else{
+            console.log('Perimeter : ' + data.perimeter() + ', Area : ' + data.area()) ;
+        }
+    })
 }
 
 solveRectangle(10,25);
-solveRectangle(0,4);
+// solveRectangle(0,4);
--- shapes.js
+++ shapes.js
@@ -1,12 +1,28 @@
-let shapes = {
-    rectangle:{
-        perimeter : (x,y) => {
-            return 2 * (x+y)  ;
-        },
-        area : (x,y) => {
-            return x*y;
-        }
+module.exports = (x,y,callback) => {
+    // the call back function has 2 params : 1 -> if error , 2-> if return value
+    // which i implemented in calling the module.
+    if(x <=0 || y <= 0 ){
+       // we will simulate a 2 seconds delay
+        setTimeout( () => {
+            callback (new Error('Rectangle dimensions should be greater than Zero : l= ' + x + ', b = ' + y) , null) ; // no return value if error
+        },2000)
     }
+    else {
+        // we can define it after the callback, only because of the delay, but better to have it first for better code read.
+        let rectangle = {
+            perimeter : () => { // if we define it as function, we should call it as function.
+                return 2 * (x+y) ;
+            } ,
+            area : () => {
+                return x*y ;
+            }
+        };
+
+        setTimeout( () => {
+            callback (null , rectangle)
+        },2000) ;
+    }
+
+
 };
 
-exports.rectangle = shapes.rectangle ; // rectangle is variable used where this file is imported.
