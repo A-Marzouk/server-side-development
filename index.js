bad52852fea5c895dece97201c0b8bc9d925388c
Ahmed-pc <perfect_ahmed@yahoo.com>
2019-07-27 18:43:45 +0300

    Express example - fetch static files

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express  = require('express'); // third part node module express.
 const http     = require('http'); // core module
+const morgan  = require('morgan'); // express middleware called morgan, used to log in information to the screen(console).
 
 const hostname = 'localhost';
 const port     = '3000';
 
 const app  = express() ;
+app.use(morgan('dev')); // will log info on screen
 
-app.use( (req, res, next) => {  // here we will call a function that will set our server.
-    console.log(req.headers) ;
+// serve static files from public folder
+// look for public in the root folder.
+// this by default will load index.html file
+app.use(express.static(__dirname + '/public'));
+
+
+// we might not need this if we already use the express static to get files.
+app.use( (req, res, next) => {
+    // here we will call a function that will set our server.
+    // this by default will be called if we have a 404 error.
     res.statusCode = 200 ;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Express server</h1></body></html>');
+    res.end('<html><body><h1>This is an express server</h1></body></html>');
 });
+
 
 const server = http.createServer(app) ;
 
